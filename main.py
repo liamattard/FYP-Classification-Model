b6e5d8a9085e4cf89c434458aee6a0ceea01d042
liamattard <liam.dratta@gmail.com>
2020-11-10 16:34:03 +0100

    small c

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from dataHandling import images
 from dataHandling import dataset 
 from cnnModel import model
+from tensorflow import keras
 
-data = images.getImages('../FYP-Dataset')
+data = images.getImages('../dataset')
 # images.showSampleImage(data,"beach",2)
 
 batch_size = 32
 img_height = 180
 img_width = 180
 
-train,test = dataset.dataset_split(batch_size,img_height,img_width,data)
-compiled_model = model.compile(img_height,img_width)
-model.train(train,test,compiled_model)
+# train,test = dataset.dataset_split(batch_size,img_height,img_width,data)
+# compiled_model = model.compile(img_height,img_width)
+# model.train(train,test,compiled_model)
+model = keras.models.load_model('my_model.hdf5')
 
 
